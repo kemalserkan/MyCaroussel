0850d94ee7cb4e2412d88f20229684f191386335
Kemal Serkan YILDIRIM <kemal.yildirim@thy.com>
2018-07-19 09:08:07 +0300

    - Changed SWIFT_VERSION to 4.0 - Run at Xcode 9

--- MyCaroussel/Carrousel/CarouselView.swift
+++ MyCaroussel/Carrousel/CarouselView.swift
@@ -31,7 +31,7 @@ public class CollectionViewCell: UICollectionViewCell {
     
     func setup()
     {
-        self.pageLabel = UILabel(frame: CGRectMake(0, 0, self.frame.size.width, self.frame.size.height))
+        self.pageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: self.frame.size.height))
         self.addSubview(self.pageLabel);
     }
 }
@@ -83,13 +83,13 @@ class CarouselView: UIView {
     
     private func configureCollectionView() {
         
-        self.collectionView = UICollectionView(frame: CGRectMake(0, 0, self.frame.size.width, 250), collectionViewLayout: UICollectionViewLayout())
-        self.collectionView.backgroundColor = UIColor.whiteColor()
+        self.collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height: 250) , collectionViewLayout: UICollectionViewLayout())
+        self.collectionView.backgroundColor = UIColor.white
         self.collectionView.dataSource = self
         self.collectionView.delegate = self
         self.collectionView.showsHorizontalScrollIndicator = false
-        self.collectionView.registerClass(CollectionViewCell.self, forCellWithReuseIdentifier: cellId)
-        self.collectionViewLayout = LGHorizontalLinearFlowLayout.configureLayout(collectionView: self.collectionView, itemSize: CGSizeMake(180, 180), minimumLineSpacing: 0)
+        self.collectionView.register(CollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        self.collectionViewLayout = LGHorizontalLinearFlowLayout.configureLayout(collectionView: self.collectionView, itemSize: CGSize(width: 180, height: 180), minimumLineSpacing: 0)
         self.addSubview(self.collectionView)
         
     }
@@ -97,44 +97,44 @@ class CarouselView: UIView {
     private func configurePageControl() {
         
         let y = self.collectionView.frame.origin.y + self.collectionView.frame.size.height //self.frame.size.height-20
-        self.pageControl = UIPageControl(frame: CGRectMake(0, y, self.frame.size.width, 20))
-        self.pageControl.backgroundColor = UIColor.grayColor()
+        self.pageControl = UIPageControl(frame: CGRect(x: 0, y: y, width: self.frame.size.width, height: 20) )
+        self.pageControl.backgroundColor = UIColor.gray
         self.addSubview(self.pageControl)
         self.pageControl.numberOfPages = self.dataSource.count
-        self.pageControl.addTarget(self, action:#selector(pageControlValueChanged), forControlEvents: UIControlEvents.ValueChanged)
+        self.pageControl.addTarget(self, action:#selector(pageControlValueChanged), for: UIControlEvents.valueChanged)
     }
     
     
     // MARK: Actions
     
     @objc private func pageControlValueChanged(sender: AnyObject) {
-        self.scrollToPage(self.pageControl.currentPage, animated: true)
+        self.scrollToPage(page: self.pageControl.currentPage, animated: true)
     }
 
     private func scrollToPage(page: Int, animated: Bool) {
-        self.collectionView.userInteractionEnabled = false
+        self.collectionView.isUserInteractionEnabled = false
         self.animationsCount += 1
         let pageOffset = CGFloat(page) * self.pageWidth - self.collectionView.contentInset.left
-        self.collectionView.setContentOffset(CGPointMake(pageOffset, 0), animated: true)
+        self.collectionView.setContentOffset(CGPoint(x: pageOffset, y: 0), animated: true)
         self.pageControl.currentPage = page
     }
 }
 
 extension CarouselView: UICollectionViewDataSource, UICollectionViewDelegate {
     
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.dataSource.count
     }
     
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let collectionViewCell = collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath) as! CollectionViewCell
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let collectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as! CollectionViewCell
         collectionViewCell.pageLabel.text = self.dataSource[indexPath.row]
-        collectionViewCell.backgroundColor = UIColor.greenColor()
+        collectionViewCell.backgroundColor = UIColor.green
         return collectionViewCell
     }
     
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        if collectionView.dragging || collectionView.decelerating || collectionView.tracking {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView.isDragging || collectionView.isDecelerating || collectionView.isTracking {
             return
         }
         
@@ -144,18 +144,18 @@ extension CarouselView: UICollectionViewDataSource, UICollectionViewDelegate {
             NSLog("Did select center item")
         }
         else {
-            self.scrollToPage(selectedPage, animated: true)
+            self.scrollToPage(page: selectedPage, animated: true)
         }
     }
     
-    func scrollViewDidEndDecelerating(scrollView: UIScrollView) {
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         self.pageControl.currentPage = Int(self.contentOffset / self.pageWidth)
     }
     
-    func scrollViewDidEndScrollingAnimation(scrollView: UIScrollView) {
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
         self.animationsCount -= 1;
         if self.animationsCount == 0 {
-            self.collectionView.userInteractionEnabled = true
+            self.collectionView.isUserInteractionEnabled = true
         }
     }
     
